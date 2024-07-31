ca36d403c84849df3950c13f15b83b7d766c87c1
Giovanni Ghirardelli <gianni_96@hotmail.it>
2024-07-31 18:21:28 +0200

    minorfix

--- src/components/MyHomePage.jsx
+++ src/components/MyHomePage.jsx
@@ -1,4 +1,4 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Placeholder, Row } from "react-bootstrap";
 import MyNavbar from "./MyNavbar";
 import ResevationWidget from "./ReservationWidget";
 import WorkShiftWidget from "./WorkShiftWidget";
@@ -6,24 +6,35 @@ import ReservationQuickButton from "./ReservationQuickButton";
 import WaitStaffWidget from "./WaitStaffWidget";
 import KitchenWidget from "./KitchenWidget";
 import MaulEe from "../assets/EE_white.svg";
+import { useSelector } from "react-redux";
 
 const MyHomePage = () => {
+  const me = useSelector((state) => state.user.state);
   return (
     <>
       <MyNavbar />
-      <Container className="text-white">
-        <Row className="">
-          <div className="d-flex justify-content-between my-3">
-            <h1>Dashboard Giornaliera: </h1>
-            <img className="right-logo" src={MaulEe} alt="Alt logo" />
-          </div>
-          <WorkShiftWidget />
-          <ResevationWidget />
-          <WaitStaffWidget />
-          <KitchenWidget />
-          <ReservationQuickButton />
-        </Row>
-      </Container>
+      {!me ? (
+        <>
+          <Placeholder xs={12} size="lg" />
+          <Placeholder xs={12} />
+          <Placeholder xs={12} size="sm" />
+          <Placeholder xs={12} size="xs" />
+        </>
+      ) : (
+        <Container className="text-white">
+          <Row className="">
+            <div className="d-flex justify-content-between my-3">
+              <h1>Dashboard Giornaliera: </h1>
+              <img className="right-logo" src={MaulEe} alt="Alt logo" />
+            </div>
+            <WorkShiftWidget />
+            <ResevationWidget />
+            <WaitStaffWidget />
+            <KitchenWidget />
+            <ReservationQuickButton />
+          </Row>
+        </Container>
+      )}
     </>
   );
 };
