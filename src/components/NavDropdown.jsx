c9a9b59eac34437c787eb554badeaac77e92d9cd
Giovanni Ghirardelli <gianni_96@hotmail.it>
2024-08-01 11:17:34 +0200

    minor fix

--- src/components/NavDropdown.jsx
+++ src/components/NavDropdown.jsx
@@ -1,4 +1,10 @@
-import { Dropdown, DropdownMenu, Nav, NavDropdown } from "react-bootstrap";
+import {
+  Dropdown,
+  DropdownMenu,
+  Nav,
+  NavDropdown,
+  Placeholder,
+} from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -7,43 +13,52 @@ const NavDropDown = () => {
 
   return (
     <Nav>
-      <Dropdown className="d-lg-none ">
-        <Dropdown.Toggle
-          style={{
-            background: "linear-gradient(to right, #fad200, #f6921e)",
-          }}
-          className="d-flex text-dark align-items-center p-2 textfor rounded-pill"
-          id="dropdown-basic"
-        >
-          {me && me.name ? me.name : "Utente"}
-        </Dropdown.Toggle>
+      {!me ? (
+        <>
+          <Placeholder xs={12} size="lg" />
+          <Placeholder xs={12} />
+          <Placeholder xs={12} size="sm" />
+          <Placeholder xs={12} size="xs" />
+        </>
+      ) : (
+        <Dropdown className="d-lg-none ">
+          <Dropdown.Toggle
+            style={{
+              background: "linear-gradient(to right, #fad200, #f6921e)",
+            }}
+            className="d-flex text-dark align-items-center p-2 textfor rounded-pill"
+            id="dropdown-basic"
+          >
+            {me && me.name ? me.name : "Utente"}
+          </Dropdown.Toggle>
 
-        <DropdownMenu className="position-absolute">
-          <Link to={"/profilo"}>
-            <NavDropdown.Item href="#action/3.1">Profilo</NavDropdown.Item>
-          </Link>
-          {["GESTORE", "ADMIN"].includes(me.role) ? (
-            <>
-              <Link to={"/personale"}>
-                <NavDropdown.Item href="#action/3.2">
-                  Gestione Staff
-                </NavDropdown.Item>
-              </Link>
-              <Link to={"/liste"}>
-                <NavDropdown.Item href="#action/3.3">
-                  Riepilogo Liste
-                </NavDropdown.Item>
-              </Link>
-            </>
-          ) : (
-            <></>
-          )}
-          <NavDropdown.Divider />
-          <Link to={"/"}>
-            <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
-          </Link>
-        </DropdownMenu>
-      </Dropdown>
+          <DropdownMenu className="position-absolute">
+            <Link to={"/profilo"}>
+              <NavDropdown.Item href="#action/3.1">Profilo</NavDropdown.Item>
+            </Link>
+            {["GESTORE", "ADMIN"].includes(me.role) ? (
+              <>
+                <Link to={"/personale"}>
+                  <NavDropdown.Item href="#action/3.2">
+                    Gestione Staff
+                  </NavDropdown.Item>
+                </Link>
+                <Link to={"/liste"}>
+                  <NavDropdown.Item href="#action/3.3">
+                    Riepilogo Liste
+                  </NavDropdown.Item>
+                </Link>
+              </>
+            ) : (
+              <></>
+            )}
+            <NavDropdown.Divider />
+            <Link to={"/"}>
+              <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
+            </Link>
+          </DropdownMenu>
+        </Dropdown>
+      )}
     </Nav>
   );
 };
